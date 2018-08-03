6c554972addca86e2c52228ed8310b160bb824f0
sevenflash <sevenflash@yandex.ru>
2018-08-03 20:57:28 +0300

    example

---
+++ example.js
@@ -0,0 +1,15 @@
+const Web3Wallet = require('web3-wallet');
+const { Web3EventStream } = require('web3-stream');
+const RedisStream = require('./index.js')
+const redis = require("fakeredis")
+
+const web3 = Web3Wallet.create(null, 'https://api.myetherapi.com/eth');
+const contract = web3.eth.loadContract(require('./CryptoKitties.json'), '0x06012c8cf97bead5deae237070f9587f8e7a266d');
+
+const redisClient = redis.createClient({fast: false})
+const collectionName = 'ethbtc'
+
+const web3Stream = new Web3EventStream(web3, contract.Transfer, {}, { fromBlock: 4605167 })
+const redisStream = new RedisStream({ redisClient, collectionName })
+
+web3Stream.pipe(redisStream)
